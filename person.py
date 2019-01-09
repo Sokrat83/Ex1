0f35f4f385385993f015f7e400278646b0bd3899
Kovalyov Boris <boris.ko83@gmail.com>
2019-01-09 11:33:06 +0600

    Add files via upload

--- person.py
+++ person.py
@@ -10,13 +10,27 @@ class Person:
     def giveRaise(self, percent):
         self.pay = int(self.pay * (1 + percent))
 
+    def __str__(self):
+        return '[Person: %s, %s]' % (self.name, self.pay)
+
+class Manager(Person):
+
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent+bonus)
+
+
 
 bob = Person('Bob Smith')
 sue = Person('Sue Jones', job='dev', pay=100000)
-print(bob.name, bob.pay)
-print(sue.name, sue.pay)
+print(bob)
+print(sue)
 print(bob.lastName(), sue.lastName())
 sue.giveRaise(.10)
 print(sue.pay)
+
+tom = Manager('Tom Jones', 'mgr', 50000)
+tom.giveRaise(.10)
+print(tom.lastName())
+print(tom)
 
 
